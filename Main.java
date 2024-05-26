70e18a067c084b30a13ec423a9444752889918f2
omraval07 <151905352+omraval07@users.noreply.github.com>
2024-05-26 19:27:28 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,11 @@
-package employee;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+        public static void main(String[] args) {
+            Employee emp1 = new Employee("E001", "xyz@asd.com");
+            System.out.println(emp1);
+            emp1.setEmpId("E002");
+            System.out.println(emp1);
+        }
 
-    public static void main(String[] args) {
-        Employee emp1 = new Employee(101, "Darshan", 21, 35000);
-        Employee emp2 = new Employee(102, "Ashwin", 22, 45000);
-        Employee emp3 = new Employee(103, "Talati", 20, 19500);
-
-        emp1.setTax(CalculateEmpTax.calucateTax(emp1));
-        emp2.setTax(CalculateEmpTax.calucateTax(emp2));
-        emp3.setTax(CalculateEmpTax.calucateTax(emp3));
-
-        System.out.println(emp1);
-        System.out.println(emp2);
-        System.out.println(emp3);
-    }
 }
